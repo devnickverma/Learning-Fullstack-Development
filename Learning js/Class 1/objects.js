12107cb434becb0c4f5dc754cb98001777a425ae
Nikhil Verma <devnickverma@gmail.com>
2024-05-19 21:33:35 +0530

    Add js folder

---
+++ Learning js/Class 1/objects.js
@@ -0,0 +1,16 @@
+const user1 = [{
+    fname: "Nikhil",
+    gender: "Male",
+  },{
+    fname: "Priya",
+    gender: "Female",
+  },{
+  fname: "Rahul",
+  gender: "Male",
+}]
+ 
+for(let i=0; i<user1.length; i++){
+  if(user1[i]["gender"] == "Male"){
+    console.log(user1[i]["fname"]);
+  }
+}
